e45ebde14fba5fac36238fc3e1ade584514413ba
ZunairaMughal24 <168114331+ZunairaMughal24@users.noreply.github.com>
2025-02-28 05:35:38 -0800

    textfields validation

--- lib/widgets/BeautifulView.dart
+++ lib/widgets/BeautifulView.dart
@@ -1,42 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class BeautifulView extends StatelessWidget {
-//   final String title;
-//   final Color? color;
-//   final double? size;
-//   final double? borderRadius;
-//   final EdgeInsets? padding;
-//   final String fontFamily;
-
-//   const BeautifulView({
-//     required this.title,
-//     this.color = Colors.blue,
-//     this.size,
-//     this.borderRadius,
-//     this.padding,
-//     this.fontFamily = "sana",
-//     super.key,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: padding ?? EdgeInsets.all(16),
-//       decoration: BoxDecoration(
-//         color: const Color.fromARGB(255, 129, 109, 109),
-//         borderRadius: BorderRadius.circular(borderRadius ?? 0),
-//       ),
-//       child: Text(
-//         title,
-//         style: TextStyle(
-//           fontSize: size ?? 16,
-//           color: Colors.black,
-//           fontFamily: fontFamily,
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -251,49 +212,6 @@ class _BeautifulViewState extends State<BeautifulView> {
               )
             ],
           ),
-        )
-        // child: Container(
-        //   padding: EdgeInsets.all(double.tryParse(padding) ?? 16),
-        //   decoration: BoxDecoration(
-        //     color: const Color.fromARGB(255, 175, 161, 161),
-        //     borderRadius:
-        //         BorderRadius.circular(double.tryParse(borderRadius) ?? 12),
-        //   ),
-        //   child: Column(
-        //     mainAxisSize: MainAxisSize.min,
-        //     children: [
-        //       Text(
-        //         title,
-        //         style: TextStyle(
-        //           fontSize: 16,
-        //           fontWeight: FontWeight.bold,
-        //           color: Colors.white,
-        //         ),
-        //       ),
-        //       const SizedBox(height: 12),
-        //       ElevatedButton(
-        //         style: ElevatedButton.styleFrom(
-        //           backgroundColor: Colors.white,
-        //           foregroundColor: Colors.black,
-        //           padding:
-        //               const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-        //           shape: RoundedRectangleBorder(
-        //             borderRadius: BorderRadius.circular(8),
-        //           ),
-        //         ),
-        //         onPressed: () {},
-        //         child: Text(
-        //           buttonText,
-        //           style: TextStyle(
-        //             fontSize: 16,
-        //             fontWeight: FontWeight.w600,
-        //           ),
-        //         ),
-        //       ),
-        //     ],
-        //   ),
-        // ),
-
-        );
+        ));
   }
 }
